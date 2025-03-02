641bac0997e73684e933075cd2749d0a14877ea7
aikdp <kdprasad028@gmail.com>
2025-03-02 15:47:52 +0530

    docker practice

--- docker.sh
+++ docker.sh
@@ -23,16 +23,24 @@ CHECK
 
 
 dnf -y install dnf-plugins-core
+VALIDATE $? "Installing Docker plugins"
  
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+VALIDATE $? "Installing Docker Repo"
 
 dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+VALIDATE $? "Installing Docker Compose Plugins"
 
 systemctl start docker
+VALIDATE $? "Start Docker"
 
 usermod -aG docker ec2-user
 VALIDATE $? "User added in Docker group"
 
 docker run hello-world
+VALIDATE $? "Response from Docker"
 
 docker --version
+VALIDATE $? "Displaying Docker Version"
+
+echo "Exit and Login Agian will work Docker commands. Thanks"
